43ea957abc578bf54c6775ffe7ff615aa5c1aac6
nlankwei5 <lampteyebenezer220@gmail.com>
2025-03-12 10:23:51 +0000

    app.js modified
    Game logic was created and scoring as well

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
+function playRound(computerChoice, humanChoice){
+    if (computerChoice.toLowerCase() === "rock" && humanChoice.toLowerCase() === "paper"){
+        console.log("You win! Paper beats Rock")
+        humanScore += 1;
+        return
+    }else if (computerChoice.toLowerCase() === "scissors" && humanChoice.toLowerCase() === "paper"){
+        console.log("You lose! Scissors beats Paper")
+        computerScore += 1;
+        return
+   }else if (computerChoice.toLowerCase() === "paper" && humanChoice.toLowerCase() === "scissors"){
+        console.log("You win! Scissors beats Paper")
+        humanScore += 1;
+        return
+   }else if (computerChoice.toLowerCase() === "paper" && humanChoice.toLowerCase() === "rock"){
+        console.log("You lose! Paper beats Rock")
+        computerScore += 1;
+        return
+    }else {
+        console.log("Draw! Replay")
+    }
+}
+
+
+//playRound(computerSelection,humanSelection)
 
 let humanScore = 0;
 let computerScore = 0;
 
-function computerChoice(){
+function getComputerChoice(){
     random = Math.floor(Math.random()*3)
-    if (random == 0) {
+    if (random === 0) {
         return "Rock"
-    }else if (random == 1){
+    }else if (random === 1){
         return "Paper"
     }else {
         return "Scissors"
     }
 }
 
-let choice = (humanChoice) => {return prompt("Rock, Paper, Scissors: ") }
+let getHumanChoice = (choice) => {return prompt("Rock, Paper, Scissors: ") }
 
-console.log(choice())
+const computerSelection = getComputerChoice()
+const humanSelection = getHumanChoice()
+
+console.log(playRound(computerSelection,humanSelection))
+console.log("Your Score is:", humanScore)
+console.log("Computer Score is:", computerScore)
+
+
